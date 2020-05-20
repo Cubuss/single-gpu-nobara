fa692f9ee6df7050c787f3622d72367cd0f3e100
RisingPrism <git@risingprism.com>
2020-05-20 14:12:40 -0400

    Minor tweaks

--- polaris-vfio-startup.sh
+++ polaris-vfio-startup.sh
@@ -2,23 +2,19 @@
 # Helpful to read output when debugging
 set -x
 
-# Stop display manager
-#systemctl stop x11vnc.service
+# Stop display manager (KDE specific)
 systemctl stop sddm.service
 
 # Unbind VTconsoles
 echo 0 > /sys/class/vtconsole/vtcon0/bind
-#echo 0 > /sys/class/vtconsole/vtcon1/bind
-
 
 # Unbind EFI-Framebuffer
-#echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
 sleep 5
 
 # Unload AMD drivers
 modprobe -r amdgpu
-
 
 # Unbind the GPU from display driver
 virsh nodedev-detach pci_0000_01_00_0
