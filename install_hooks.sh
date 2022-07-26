d37ebbeb11518421720e9341aaf16707bbdaac8f
Lily Freeman <syntax2056@gmail.com>
2022-07-26 00:14:25 +0000

    Update install_hooks.sh

--- install_hooks.sh
+++ install_hooks.sh
@@ -8,13 +8,13 @@ if test -e /etc/libvirt/hooks/qemu;
 then
     mv /etc/libvirt/hooks/qemu /etc/libvirt/hooks/qemu_last_backup
 fi
-if test -e /bin/vfio-startup.sh;
+if test -e /usr/local/bin/vfio-startup.sh;
 then
-    mv /bin/vfio-startup.sh /bin/vfio-startup.sh.bkp
+    mv /usr/local/bin/vfio-startup.sh /usr/local/bin/vfio-startup.sh.bkp
 fi
-if test -e /bin/vfio-teardown.sh;
+if test -e /usr/local/bin/vfio-teardown.sh;
 then
-    mv /bin/vfio-teardown.sh /bin/vfio-teardown.sh.bkp
+    mv /usr/local/bin/vfio-teardown.sh /usr/local/bin/vfio-teardown.sh.bkp
 fi
 if test -e /etc/systemd/system/libvirt-nosleep@.service;
 then
@@ -22,10 +22,10 @@ then
 fi
 
 cp systemd-no-sleep/libvirt-nosleep@.service /etc/systemd/system/libvirt-nosleep@.service
-cp hooks/vfio-startup.sh /bin/vfio-startup.sh
-cp hooks/vfio-teardown.sh /bin/vfio-teardown.sh
+cp hooks/vfio-startup.sh /usr/local/bin/vfio-startup.sh
+cp hooks/vfio-teardown.sh /usr/local/bin/vfio-teardown.sh
 cp hooks/qemu /etc/libvirt/hooks/qemu
 
-chmod +x /bin/vfio-startup.sh
-chmod +x /bin/vfio-teardown.sh
+chmod +x /usr/local/bin/vfio-startup.sh
+chmod +x /usr/local/bin/vfio-teardown.sh
 chmod +x /etc/libvirt/hooks/qemu
