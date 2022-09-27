907f18d50c0a773ce4e435aa9e756402f25e9937
Cubuss <9220880+Cubuss@users.noreply.github.com>
2022-09-27 23:56:17 +0200

    Update vfio-teardown.sh

--- hooks/vfio-teardown.sh
+++ hooks/vfio-teardown.sh
@@ -1,99 +1,30 @@
-#!/bin/bash
+  GNU nano 6.0                                                                    vfio-teardown.sh                                                                              
+!/bin/bash
+set -x
 
-#############################################################################
-##     ______  _                _  _______         _                 _     ##
-##    (_____ \(_)              | |(_______)       | |               | |    ##
-##     _____) )_  _   _  _____ | | _    _   _   _ | |__   _____   __| |    ##
-##    |  ____/| |( \ / )| ___ || || |  | | | | | ||  _ \ | ___ | / _  |    ##
-##    | |     | | ) X ( | ____|| || |__| | | |_| || |_) )| ____|( (_| |    ##
-##    |_|     |_|(_/ \_)|_____) \_)\______)|____/ |____/ |_____) \____|    ##
-##                                                                         ##
-#############################################################################
-###################### Credits ###################### ### Update PCI ID'S ###
-## Lily (PixelQubed) for editing the scripts       ## ##                   ##
-## RisingPrisum for providing the original scripts ## ##   update-pciids   ##
-## Void for testing and helping out in general     ## ##                   ##
-## .Chris. for testing and helping out in general  ## ## Run this command  ##
-## WORMS for helping out with testing              ## ## if you dont have  ##
-##################################################### ## names in you're   ##
-## The VFIO community for using the scripts and    ## ## lspci feedback    ##
-## testing them for us!                            ## ## in your terminal  ##
-##################################################### #######################
-
-################################# Variables #################################
-
-## Adds current time to var for use in echo for a cleaner log and script ##
-DATE=$(date +"%m/%d/%Y %R:%S :")
-
-################################## Script ###################################
-
-echo "$DATE Beginning of Teardown!"
-
-## Unload VFIO-PCI driver ##
-modprobe -r vfio_pci
+modprobe -r vfio-pci
 modprobe -r vfio_iommu_type1
 modprobe -r vfio
+ 
+# change pci_0000_08_00_0 to your GPU and Audio PCI id from IOMMU group script
+virsh nodedev-reattach pci_0000_08_00_0
+virsh nodedev-reattach pci_0000_08_00_1
+ 
 
-if grep -q "true" "/tmp/vfio-is-nvidia" ; then
+echo 1 > /sys/class/vtconsole/vtcon0/bind
+nvidia-xconfig --query-gpu-info > /dev/null 2>&1
+echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+ 
 
-    ## Load NVIDIA drivers ##
-    echo "$DATE Loading NVIDIA GPU Drivers"
-    
-    modprobe drm
-    modprobe drm_kms_helper
-    modprobe i2c_nvidia_gpu
-    modprobe nvidia
-    modprobe nvidia_modeset
-    modprobe nvidia_drm
-    modprobe nvidia_uvm
+modprobe nvidia_drm
+modprobe nvidia_modeset
+modprobe nvidia_uvm
+modprobe nvidia
+modprobe ipmi_devintf
+modprobe ipmi_msghandler
+ 
 
-    echo "$DATE NVIDIA GPU Drivers Loaded"
-fi
-
-if  grep -q "true" "/tmp/vfio-is-amd" ; then
-
-    ## Load NVIDIA drivers ##
-    echo "$DATE Loading AMD GPU Drivers"
-    
-    modprobe drm
-    modprobe amdgpu
-    modprobe radeon
-    modprobe drm_kms_helper
-    
-    echo "$DATE AMD GPU Drivers Loaded"
-fi
-
-## Restart Display Manager ##
-input="/tmp/vfio-store-display-manager"
-while read -r DISPMGR; do
-  if command -v systemctl; then
-
-    ## Make sure the variable got collected ##
-    echo "$DATE Var has been collected from file: $DISPMGR"
-
-    systemctl start "$DISPMGR.service"
-
-  else
-    if command -v sv; then
-      sv start "$DISPMGR"
-    fi
-  fi
-done < "$input"
-
-############################################################################################################
-## Rebind VT consoles (adapted and modernised from https://www.kernel.org/doc/Documentation/fb/fbcon.txt) ##
-############################################################################################################
-
-input="/tmp/vfio-bound-consoles"
-while read -r consoleNumber; do
-  if test -x /sys/class/vtconsole/vtcon"${consoleNumber}"; then
-      if [ "$(grep -c "frame buffer" "/sys/class/vtconsole/vtcon${consoleNumber}/name")" \
-           = 1 ]; then
-    echo "$DATE Rebinding console ${consoleNumber}"
-	  echo 1 > /sys/class/vtconsole/vtcon"${consoleNumber}"/bind
-      fi
-  fi
-done < "$input"
+systemctl start gdm
 
 
-echo "$DATE End of Teardown!"
+
